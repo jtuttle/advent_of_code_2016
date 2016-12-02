608ee19b709df4cd57fada803d3096892c81c5b9
John Tuttle <jtuttle@freemyapps.com>
2016-12-02 09:24:07 -0500

    add first intersect find to day one

--- day_1.rb
+++ day_1.rb
@@ -2,6 +2,8 @@ input = "R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4,
 
 dir = "N"
 pos = [0, 0]
+visits = {"0,0" => 1}
+first_revisit = nil
 
 def turn(current_dir, turn_dir)
   dirs = ["N", "E", "S", "W"]
@@ -9,19 +11,19 @@ def turn(current_dir, turn_dir)
   return dirs[(current_idx + (turn_dir == "L" ? -1 : 1)) % 4]
 end
 
-def walk(pos, dir, dist)
+def step(pos, dir)
   x = pos[0]
   y = pos[1]
 
   case dir
     when "N"
-      y += dist
+      y += 1
     when "E"
-      x += dist
+      x += 1
     when "S"
-      y -= dist
+      y -= 1
     when "W"
-      x -= dist
+      x -= 1
   end
 
   [x, y]
@@ -33,9 +35,25 @@ input.each do |i|
   puts "started at: #{pos[0]},#{pos[1]} facing #{dir}"
 
   dir = turn(dir, i[0])
-  pos = walk(pos, dir, i[1..-1].to_i)
+  dist = i[1..-1].to_i
+
+  for n in (0...dist) do
+    pos = step(pos, dir)
+
+    visit_key = "#{pos[0]},#{pos[1]}"
+
+    if first_revisit.nil? && visits.include?(visit_key)
+      puts "made first revisit at: #{visit_key}"
+      first_revisit = pos
+    end
+
+    visits[visit_key] = 0 unless visits.include?(visit_key)
+    visits[visit_key] += 1
+  end
 
   puts "ended at: #{pos[0]},#{pos[1]} facing #{dir}"
 end
 
+puts "--------------------------------"
 puts "Total distance: #{pos[0].abs + pos[1].abs}"
+puts "First revisit distance: #{first_revisit[0].abs + first_revisit[1].abs}"
