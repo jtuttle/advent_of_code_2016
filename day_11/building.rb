2dc95003951dca5fa9ef7260674c29d9e05dbc84
John Tuttle <jtuttle@freemyapps.com>
2016-12-13 21:36:45 -0500

    remove floor model

--- day_11/building.rb
+++ day_11/building.rb
@@ -1,37 +1,60 @@
 class Building
   attr_reader :floors, :elevator_level
 
-  def self.clone(building)
-
-  end
-
-  def initialize
-    @elevator_level = 0
+  def initialize(elevator_level = 0)
+    @elevator_level = elevator_level
     @floors = []
     @manifest = []
   end
 
-  def is_valid?
-    !@floors.map(&:is_valid?).include?(false)
+  def valid?
+    @floors.each do |floor|
+      microchips = floor.select { |e| e.type == :microchip }
+      
+      microchips.each do |microchip|
+        endangered =
+          !floor.select { |e|
+            e.type == :generator && e.element != microchip.element
+          }.empty?
+        
+        if endangered
+          shielded  =
+            !floor.select { |e|
+              e.type == :generator && e.element == microchip.element
+            }.empty?
+          
+          return false unless shielded
+        end
+      end
+    end
+
+    true
+  end
+
+  def add_floor
+    new_floor = []
+    @floors << new_floor
+    new_floor
+  end
+
+  def add_equipment_to_floor(equipment, floor)
+    floor << equipment
+    @manifest << equipment
+    @manifest.sort_by!(&:element)
   end
 
   def parse(input)
     input.each do |line|
-      floor = []
-      @floors << floor
+      floor = add_floor
 
       line.scan(/(\w+) generator/).flatten.each do |element|
-        floor << Equipment.new(element, :generator)
+        add_equipment_to_floor(Equipment.new(element, :generator), floor)
       end
 
       line.scan(/(\w+)-compatible microchip/).flatten.each do |element|
-        floor << Equipment.new(element, :microchip)
+        add_equipment_to_floor(Equipment.new(element, :microchip), floor)
       end
-
-      @manifest.concat(floor)
     end
-
-    @manifest.sort_by!(&:element)    
   end
 
   def print
@@ -48,5 +71,19 @@ class Building
     end
 
     nil
+  end
+
+  def clone
+    clone = Building.new(@elevator_level)
+
+    @floors.each do |floor|
+      cloned_floor = clone.add_floor
+
+      floor.each do |equipment|
+        clone.add_equipment_to_floor(equipment, cloned_floor)
+      end
+    end
+    
+    clone
   end
 end
