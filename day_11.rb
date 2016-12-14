aecfa61d07c23737a462f4ee86dfab42b3910d60
John Tuttle <jtuttle@freemyapps.com>
2016-12-13 22:08:41 -0500

    rename some stuff

--- day_11.rb
+++ day_11.rb
@@ -8,8 +8,8 @@ def try_move(building, equipment, from_elevator_level, to_elevator_level)
   clone.valid? ? clone : nil
 end
 
-def generate_moves(building)
-  valid_buildings = []
+def generate_valid_moves(building)
+  valid_building_states = []
 
   elevator_level = building.elevator_level
   current_floor = building.current_floor
@@ -17,27 +17,27 @@ def generate_moves(building)
   for i in (0...current_floor.count)
     # move up
     if elevator_level < building.floors.count
-      valid_building = try_move(building, [current_floor[i]], elevator_level, elevator_level + 1)
-      valid_buildings << valid_building unless valid_building.nil?
+      building_state = try_move(building, [current_floor[i]], elevator_level, elevator_level + 1)
+      valid_building_states << building_state unless building_state.nil?
     end
 
     # move down
     if elevator_level > 0
-      valid_building = try_move(building, [current_floor[i]], elevator_level, elevator_level - 1)
-      valid_buildings << valid_building unless valid_building.nil?
+      building_state = try_move(building, [current_floor[i]], elevator_level, elevator_level - 1)
+      valid_building_states << building_state unless building_state.nil?
     end
 
     for j in (i+1...current_floor.count)
       # move up
       if elevator_level < building.floors.count
-        valid_building = try_move(building, [current_floor[i], current_floor[j]], elevator_level, elevator_level + 1)
-        valid_buildings << valid_building unless valid_building.nil?
+        building_state = try_move(building, [current_floor[i], current_floor[j]], elevator_level, elevator_level + 1)
+        valid_building_states << building_state unless building_state.nil?
       end
 
       # move down
       if elevator_level > 0
-        valid_building = try_move(building, [current_floor[i], current_floor[j]], elevator_level, elevator_level - 1)
-        valid_buildings << valid_building unless valid_building.nil?
+        building_state = try_move(building, [current_floor[i], current_floor[j]], elevator_level, elevator_level - 1)
+        valid_building_states << building_state unless building_state.nil?
       end
     end
   end
