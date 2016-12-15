863314e4859af15dc8e14f8c16f18774c550a4f6
John Tuttle <jtuttle@freemyapps.com>
2016-12-14 22:32:11 -0500

    implement priority queue for a star

--- day_13.rb
+++ day_13.rb
@@ -1,6 +1,12 @@
+require_relative 'a_star'
+
 input = 1358
 
-def is_open?(x, y, num)
+Struct.new("Point", :x, :y)
+
+def is_open?(point, num)
+  x = point.x
+  y = point.y
   (x*x + 3*x + 2*x*y + y + y*y + num).to_s(2).count('1') % 2 == 0
 end
 
@@ -8,7 +14,8 @@ def draw_floor(width, height, input)
   for y in (0...width)
     row = ""
     for x in (0...width)
-      row << (is_open?(x, y, input) ? '.' : '#')
+      point = Struct::Point.new(x, y)
+      row << (is_open?(point, input) ? '.' : '#')
     end
 
     puts row
@@ -17,4 +24,9 @@ def draw_floor(width, height, input)
   nil
 end
 
+def manhattan(p1, p2)
+  (p1.x - p2.x).abs + (p1.y - p2.y).abs
+end
+
 puts draw_floor(10, 10, 10)
+
